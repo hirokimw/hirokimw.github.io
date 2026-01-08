90cda6ca378c26b065be13d40b878c47d8068457
hirokimw <hirokiw757@gmail.com>
2026-01-08 16:17:20 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
 const btn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
 btn.addEventListener("click", () => {
   btn.classList.toggle("active");
   menu.classList.toggle("open");
-});
 });
