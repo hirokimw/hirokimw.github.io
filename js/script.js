eae4ded752d54bf27226ba37cf403f6754c592a0
hirokimw <hirokiw757@gmail.com>
2026-01-08 10:30:57 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,3 +13,26 @@ function changeColors() {
 
   index = (index + 1) % navColors.length;
 }
+
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.style.display = (i === index) ? "block" : "none";
+  });
+}
+
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % slides.length;
+  showSlide(slideIndex);
+}
+
+function prevSlide() {
+  slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+  showSlide(slideIndex);
+}
+
+// 最初のスライドを表示
+showSlide(slideIndex);
+
