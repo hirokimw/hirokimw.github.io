c6d6d314b818d5e05e4b216c0d423171f51b72fa
hirokimw <hirokiw757@gmail.com>
2026-01-08 16:22:39 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 const btn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
-btn.addEventListener("click", () => {
+// ☰ ボタン
+btn.addEventListener("click", (e) => {
+  e.stopPropagation(); // ← ここ重要
   btn.classList.toggle("active");
   menu.classList.toggle("open");
+});
+
+// メニュー内クリックは閉じない
+menu.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+// 画面のどこかを押したら閉じる
+document.addEventListener("click", () => {
+  btn.classList.remove("active");
+  menu.classList.remove("open");
 });
